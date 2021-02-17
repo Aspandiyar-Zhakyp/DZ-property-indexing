72064392b470852b52cd26bd24647d2b78efe20b
Aspandiyar-Zhakyp <75847712+Aspandiyar-Zhakyp@users.noreply.github.com>
2021-02-17 21:52:52 +0600

    DZ/ practice

---
+++ PropertyIndexersPractice/PropertyIndexersPractice/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PropertyIndexersPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Console.WriteLine("Введите нижний индекс диапазона массива");
+            int lowerIndex = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите верхний индекс диапазона массива");
+            int uperIndex = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            if (!(lowerIndex > uperIndex))
+            {
+                Console.WriteLine("Нижний индекс равен {0} верхний индекс равен {1}", lowerIndex, uperIndex);
+                RangeOfArray array = new RangeOfArray(uperIndex, lowerIndex);
+                for (int i = 0; i < array.Arr.Length; i++)
+                {
+                    array.Arr[i] = rand.Next(10);
+                }
+                array.Print(array, lowerIndex);
+            }
+            else
+            {
+                Console.WriteLine("Введены некорректные индексы");
+            }
+        }
+    }
+}
---
+++ PropertyIndexersPractice/PropertyIndexersPractice/RangeOfArrays.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PropertyIndexersPractice
+{
+    class RangeOfArray
+    {
+        public int lowerBount;
+        public int upperBount;
+        public int[] Arr;
+        public RangeOfArray(int upperIndex, int lowerIndex)
+        {
+            this.lowerBount = lowerIndex;
+            this.upperBount = upperIndex;
+            Arr = new int[Math.Abs(upperIndex - lowerIndex) + 1];
+        }
+        public void Print(RangeOfArray mass, int lowerIndex)
+        {
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                Console.WriteLine($"{i + lowerIndex} элемент массива равен = " + Arr[i]);
+            }
+        }
+    }
+}
